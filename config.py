53266eaa753c6447110e9a58d279a8fa3af35c5f
pratik03071995 <108683914+pratik03071995@users.noreply.github.com>
2025-07-04 15:18:56 -0400

    mypush

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load from .env
 
 GITHUB_REPO_URL = os.getenv("GITHUB_REPO_URL")
-NOTEBOOK_PATH = os.getenv("NOTEBOOK_PATH")  # e.g., simple_calculator.ipynb
+NOTEBOOK_PATH = os.getenv("NOTEBOOK_PATH")  # e.g., Simple_calculator.ipynb
 
 CONFLUENCE_BASE_URL = os.getenv("CONFLUENCE_BASE_URL")
 CONFLUENCE_PAGE_ID = os.getenv("CONFLUENCE_PAGE_ID")
